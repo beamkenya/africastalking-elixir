0cc22c2db0d9d07044e55da40a6a2575597898f7
Zacck Osiemo <coderv63@gmail.com>
2019-05-03 13:50:58 +0200

    Implement multiple reponses handler and format code

--- lib/at_ex/USSD.ex
+++ lib/at_ex/USSD.ex
@@ -1,33 +1,36 @@
 defmodule AtEx.USSD do
-    @moduledoc """
-    This module handles building valid response for the Africas Talking USSD endpoint
-    """
+  @moduledoc """
+  This module handles building valid response for the Africas Talking USSD endpoint
+  """
 
-    @doc """
-    This function accepts a list of string and proceeds to build a valid response for
-    the AT API
-    """
-    @spec build_response(list(String.t())) :: {:ok, String.t()} | {:error, term()}
-    def build_response(responses) do
-        case responses do
-            val when length(val) > 1  ->
-                [title | responses ] = val
+  @doc """
+  This function accepts a list of string and proceeds to build a valid response for
+  the AT API
+  """
+  @spec build_response(list(String.t())) :: {:ok, String.t()} | {:error, term()}
+  def build_response(responses) do
+    case responses do
+      val when length(val) > 1 ->
+        [title | responses] = val
 
-                title = "CON " <>  title
+        title = "CON " <> title
 
-                list_resp =
-                for i <- 0..length(responses) - 1, do:  "#{i + 1}. #{ Enum.at(responses, i)} "
+        numbered_answers =
+          for i <- 0..(length(responses) - 1), do: "#{i + 1}. #{Enum.at(responses, i)}"
 
+        response =
+          [title | numbered_answers]
+          |> Enum.join(" \n ")
 
-            val when length(val) ==  1 ->
-                resp = "END " <> hd(val)
+        {:ok, response}
 
-                {:ok, resp}
+      val when length(val) == 1 ->
+        resp = "END " <> hd(val)
 
-            val when length(val) < 1  ->
-                {:error, "Responses can't be empty"}
-        end
+        {:ok, resp}
+
+      val when length(val) < 1 ->
+        {:error, "Responses can't be empty"}
     end
+  end
 end
-
-
