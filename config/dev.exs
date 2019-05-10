1b2c73583b60c66e9d32e2afe5964a5d5031139e
Zacck Osiemo <coderv63@gmail.com>
2019-05-10 12:39:40 +0200

    Add configuration options to enable standard request headers

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,9 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-
 config :at_ex,
-    api_key: "6f2a021cbd7b2b0f5e9ba324f42dbf42403ab9a239d1c55c5a68cf54922bda77",
-    content_type: "application/json",
-    accept: "application/json",
-    auth_token: "",
-    endpoint: "sandbox"
+  api_key: "6f2a021cbd7b2b0f5e9ba324f42dbf42403ab9a239d1c55c5a68cf54922bda77",
+  content_type: "application/json",
+  accept: "application/json",
+  auth_token: "",
+  endpoint: "sandbox"
