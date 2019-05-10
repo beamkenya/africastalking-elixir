3fc0a17b263a46a986fc5af69a33a8b9d634a8a1
Zacck Osiemo <coderv63@gmail.com>
2019-05-10 12:42:11 +0200

    Add tesla and jason to handle http processing

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule AtEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:tesla, "~> 1.2.1"},
+      {:jason, "~> 1.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
