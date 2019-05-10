dd2a91c9a2ae4a7e1207dc583003bc471d26c573
Zacck Osiemo <coderv63@gmail.com>
2019-05-10 12:38:46 +0200

    Add configuration options to enable standard request headers

---
+++ config/test.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+
+config :at_ex,
+    api_key: "6f2a021cbd7b2b0f5e9ba324f42dbf42403ab9a239d1c55c5a68cf54922bda77",
+    content_type: "application/json",
+    accept: "application/json",
+    auth_token: ""
