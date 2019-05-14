fa292500642722ea36b3248c97f801df0f4c676b
WanjikuMac <edwardscaroline32@gmail.com>
2019-05-14 21:18:10 +0300

    added tesla dependacy

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule AtEx.MixProject do
   defp deps do
     [
       {:tesla, "~> 1.2.1"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
