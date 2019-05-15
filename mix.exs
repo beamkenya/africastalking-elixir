cee6f8e5abcd7b2ad8c8427a2ca5a05438a2c291
Zacck Osiemo <coderv63@gmail.com>
2019-05-15 08:19:03 +0200

    Install requisite deps

--- mix.exs
+++ mix.exs
@@ -23,8 +23,6 @@ defmodule AtEx.MixProject do
     [
       {:tesla, "~> 1.2.1"},
       {:jason, "~> 1.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
