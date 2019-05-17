95e844c12737639477763c625c8343adba4617cc
Zacck Osiemo <coderv63@gmail.com>
2019-05-17 08:01:25 +0200

    Build send sms using plug and make the api usage simpler

--- lib/at_ex/gateway/sms.ex
+++ lib/at_ex/gateway/sms.ex
@@ -1,90 +1,42 @@
 defmodule AtEx.Gateway.Sms do
   @moduledoc """
   This module holds the implementation for the HTTP Gateway that runs calls against the Africas Talking API
+  SMS endpoint, use it to POST and GET requests to the SMS endpoint
   """
+  use Tesla
 
-  @username Application.get_env(:at_ex, :username)
+  @accept Application.get_env(:at_ex, :accept)
+  @key Application.get_env(:at_ex, :api_key)
+
+  plug(Tesla.Middleware.BaseUrl, "https://api.sandbox.africastalking.com/version1")
+  plug(Tesla.Middleware.FormUrlencoded)
+  plug(Tesla.Middleware.Headers, [{"accept", @accept}, {"apikey", @key}])
 
   @doc """
-  Sends HTTP Post request to the AT API to send out SMSes
+  This function builds and runs a post request to send an SMS via the Africa's talking SMS endpoint, this 
+  function accepts a map of parameters that should always contain  the `to` address and the `message` to be
+  sent
 
-  ## Parametets
-  - params: A map of parameters containing valid Sms Send inputs refer to https://build.at-labs.io/docs/sms%2Fsending
+  ## Parameters 
+  attrs: - a map containing a `to` and `message` key optionally it may also contain `from`, bulk_sms, enqueue, key_word
+  link_id and retry_hours keys, see the docs at https://build.at-labs.io/docs/sms%2Fsending for how to use these keys
   """
-  @spec send_sms(map()) :: {:ok, map()} | {:error, term()}
-  def send_sms(params) do
-    query =
-      params
-      |> Map.put(:username, @username)
-      |> Map.to_list()
+  @spec send_sms(map()) :: {:ok, term()} | {:error, term()}
+  def send_sms(attrs) do
+    username = Application.get_env(:at_ex, :username)
 
-    client = build_client()
+    params =
+      attrs
+      |> Map.put(:username, username)
 
-    Tesla.post(client, "/messaging", query: query)
-    |> process_result()
-  end
-
-  @doc """
-  Sends a HTTP GET request to collect the messages in the applications inbox, this function  accepts a 
-  map of parameters containing the last received id and an apploication username
-
-  ## Paramters 
-  map: - A map containing the last_received_id key for the last message the default is 0
-  """
-  @spec fetch_sms(map()) :: {:ok, term()} | {:error, term()}
-  def fetch_sms(params) do
-    query =
-      params
-      |> Map.put(:username, @username)
-      |> Map.to_list()
-
-    client = build_client()
-
-    Tesla.get(client, "/messaging", query: query)
-    |> process_result()
-  end
-
-  @spec process_result(Tesla.Env.t()) :: {:ok, term()} | {:error, term}
-  defp process_result(result) do
-    with {:ok, %{status: 200} = resp} <- result do
-      {:ok, resp.body}
+    with {:ok, %{status: 201} = res} <- post("/messaging", params) do
+      {:ok, Jason.decode!(res.body)}
     else
-      {:error, val} ->
-        {:error, val}
+      {:ok, val} ->
+        {:error, %{status: val.status, message: val.body}}
 
-      {:ok, resp} ->
-        {:error, resp.body}
+      {:error, message} ->
+        {:error, message}
     end
-  end
-
-  @spec build_client() :: Tesla.Client.t()
-  defp build_client() do
-    env = Application.get_env(:at_ex, :endpoint)
-
-    base_url =
-      case env do
-        "sandbox" ->
-          "https://api.sandbox.africastalking.com/version1"
-
-        "live" ->
-          "https://api.africastalking.com/version1"
-      end
-
-    middleware = build_middleware(base_url)
-
-    Tesla.client(middleware)
-  end
-
-  defp build_middleware(url) do
-    accept = Application.get_env(:at_ex, :accept)
-    content = Application.get_env(:at_ex, :content_type)
-    key = Application.get_env(:at_ex, :api_key)
-
-    middleware = [
-      {Tesla.Middleware.BaseUrl, url},
-      {Tesla.Middleware.Headers, [{"content-type", content}, {"apiKey", key}, {"accept", accept}]}
-    ]
-
-    middleware
   end
 end
