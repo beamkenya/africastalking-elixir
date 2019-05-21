222f43d416cc9e79664033c12a4a96a03399b669
kamalogudah <mcpaul2058@gmail.com>
2019-05-21 17:11:18 +0300

    SMS tests

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+config :tesla, adapter: Tesla.Mock
+
 config :at_ex,
   api_key: "6f2a021cbd7b2b0f5e9ba324f42dbf42403ab9a239d1c55c5a68cf54922bda77",
   content_type: "application/json",
---
+++ test/at_ex/gateway/sms_test.exs
@@ -0,0 +1,69 @@
+defmodule AtEx.Gateway.SmsTest do
+  @moduledoc """
+  This module holds unit tests for the functions in the SMS gateway
+  """
+  use ExUnit.Case
+
+  alias AtEx.Gateway.Sms
+
+  @attr "username="
+  setup do
+    Tesla.Mock.mock(fn
+      %{method: :post, body: @attr} ->
+        %Tesla.Env{
+          status: 400,
+          body: "Request is missing required form field 'to'"
+        }
+
+      %{method: :post} ->
+        %Tesla.Env{
+          status: 201,
+          body:
+            Jason.encode!(%{
+              "SMSMessageData" => %{
+                "Message" => "Sent to 1/1 Total Cost: ZAR 0.1124",
+                "Recipients" => [
+                  %{
+                    "cost" => "KES 0.8000",
+                    "messageId" => "ATXid_a584c3fd712a00b7bce3c4b7b552ac56",
+                    "number" => "+254728833181",
+                    "status" => "Success",
+                    "statusCode" => 101
+                  }
+                ]
+              }
+            })
+        }
+    end)
+
+    :ok
+  end
+
+  describe "Sms Gateway" do
+    test "sends_sms/1 should send sms with required parameters" do
+      # make message details
+      send_details = %{to: "+254728833181", message: "new music"}
+
+      # run details through our code
+      {:ok, result} = Sms.send_sms(send_details)
+
+      # assert our code gives us a single element list of messages
+      [msg] = result["SMSMessageData"]["Recipients"]
+
+      # assert that message details correspond to details of set up message
+      assert msg["number"] == send_details.to
+    end
+
+    test "sends_sms/1 should error out without phone number parameter" do
+      # run details through our code
+      {:error, result} = Sms.send_sms(%{})
+
+      "Request is missing required form field 'to'" = result.message
+
+      400 = result.status
+    end
+
+    test "fetches sms" do
+    end
+  end
+end
