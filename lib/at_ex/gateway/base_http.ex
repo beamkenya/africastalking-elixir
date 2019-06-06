8bcd5b8d2b0030f22e12886aed493e0804e6fede
Zacck Osiemo <coderv63@gmail.com>
2019-06-06 17:21:08 +0200

    Add documentation for using HTTP Base and use configuration

--- lib/at_ex/gateway/base_http.ex
+++ lib/at_ex/gateway/base_http.ex
@@ -4,17 +4,40 @@ defmodule AtEx.Gateway.Base do
   """
 
   @doc """
-  Macro to import necessary code into HTTP Gateways
+  Macro to import necessary code into HTTP Gateways, This Macro accepts a list as configuration at the moment it's used 
+  configure the  HTTP Base Url. 
+
+  ## Parameters 
+  * list: list of parameters for HTTP client configuration 
+
+  ## Examples 
+
+      defmodule AtEx.Gateway.Voice do 
+        use AtEx.Gateway.Base, url: "http://test.com"
+        @username "some_username"
+
+        def collect_minutes(attrs) do 
+          params =
+          attrs
+          |> Map.put(:username, @username)
+
+          {:ok, resp} = get("/minutes", params)
+          
+          process_result(resp.body)
+        end   
+      end 
   """
-  defmacro __using__(configs) do
+  defmacro __using__(opts) do
     quote do
       use Tesla
+
+      @config unquote(opts)
 
       @accept Application.get_env(:at_ex, :accept)
       @key Application.get_env(:at_ex, :api_key)
       @content_type Application.get_env(:at_ex, :content_type)
 
-      plug(Tesla.Middleware.BaseUrl, "https://api.sandbox.africastalking.com/version1/airtime")
+      plug(Tesla.Middleware.BaseUrl, @config[:url])
       plug(Tesla.Middleware.FormUrlencoded)
 
       plug(Tesla.Middleware.Headers, [
