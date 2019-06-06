3daa57e92647d854b0a93ca2147e79f45b20c17a
Zacck Osiemo <coderv63@gmail.com>
2019-06-06 17:22:08 +0200

    Serialiaze people to Africa's Talking format

--- lib/at_ex/gateway/airtime.ex
+++ lib/at_ex/gateway/airtime.ex
@@ -3,29 +3,34 @@ defmodule AtEx.Gateway.Airtime do
   This module holds the implementation for the HTTP Gateway that runs calls against the Africas Talking API
   Application Data endpoint, use it to POST and GET requests to the Application endpoint
   """
-  use AtEx.Gateway.Base
+  use AtEx.Gateway.Base, url: "https://api.sandbox.africastalking.com/version1/airtime"
 
   @type send_input :: %{recipients: list(map())}
   @type call_return :: {:ok, term()} | {:error, term()}
 
   @doc """
-  Collects application data from Africas Talking endpoint, Use this function to collect
-  Data about your application
+  Sends airtime to one or more numbers, accepts a map of parameters contaitning a list of maps each containing a
+  phone numbers and an amount of airtime to send out to a number this is expected in the `recipients` key.
 
   ## Parameters
-  * `none`
+  * `map`: map containg a recipiests which is a list of maps each with a phone number and amount
 
   ## Examples
-      iex> AtEx.Airtime.send_airtime()
+      
+      AtEx.Airtime.send_airtime(%{recipients: [%{phone_number: +254721978097, amount: "KES 50"}]})
   """
   @spec send_airtime(send_input()) :: call_return()
   def send_airtime(%{recipients: people} = attrs) do
     username = Application.get_env(:at_ex, :username)
 
+    serialized_people =
+      people
+      |> Enum.map(fn person -> serialize_person(person) end)
+
     params =
       attrs
       |> Map.put(:username, username)
-      |> Map.put(:recipients, Jason.encode!(people))
+      |> Map.put(:recipients, Jason.encode!(serialized_people))
 
     with {:ok, %{status: 201} = res} <- post("/send", params) do
       {:ok, Jason.decode!(res.body)}
@@ -37,4 +42,11 @@ defmodule AtEx.Gateway.Airtime do
         {:error, message}
     end
   end
+
+  @doc false
+  defp serialize_person(%{phone_number: pn, amount: amt}),
+    do: %{"phoneNumber" => pn, "amount" => amt}
+
+  defp serialize_person(item),
+    do: raise(ArgumentError, message: "recipient entry: #{inspect(item)} not valid")
 end
