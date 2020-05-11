12ab07652655ec668b3c87ce2b0a4bca9090a10a
kamalogudah <mcpaul2058@gmail.com>
2020-05-11 15:43:36 +0300

    Add dev.sample.exs

---
+++ config/dev.sample.exs
@@ -0,0 +1,11 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+config :at_ex,
+  api_key: "===INSERT AFRICAS_TALKING_API_KEY HERE ===",
+  content_type: "application/x-www-form-urlencoded",
+  accept: "application/json",
+  auth_token: "",
+  username: "sandbox",
+  endpoint: "sandbox"
