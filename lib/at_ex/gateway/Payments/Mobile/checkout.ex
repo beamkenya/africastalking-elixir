8ef194f45b51640f1270b81da9c875486da5799d
manuelgeek <emashmagak@appslab.co.ke>
2020-06-30 18:30:10 +0300

    added field validations

--- lib/at_ex/gateway/Payments/Mobile/checkout.ex
+++ lib/at_ex/gateway/Payments/Mobile/checkout.ex
@@ -21,6 +21,17 @@ defmodule AtEx.Gateway.Payments.Mobile.Checkout do
 
   ## Parameters
   attrs: - a map containing a `phoneNumber`, `currencyCode` and `amount` key optionally it may also contain `providerChannel` and a map of `metadata` see the docs at https://build.at-labs.io/docs/payments%2Fmobile%2Fcheckout for how to use these keys
+
+  ## Example 
+    ```
+    iex>AtEx.Gateway.Payments.Mobile.Checkout.mobile_checkout(%{phoneNumber: "254724540039", amount: 10, currencyCode: "KES"})
+    %{
+    "description" => "Waiting for user input",
+    "providerChannel" => "525900",
+    "status" => "PendingConfirmation",
+    "transactionId" => "ATPid_bbd0bcd713e27d9201807076c6db0ed5"
+    }
+    ```
   """
   @spec mobile_checkout(map()) :: {:ok, term()} | {:error, term()}
   def mobile_checkout(attrs) do
@@ -32,8 +43,36 @@ defmodule AtEx.Gateway.Payments.Mobile.Checkout do
       |> Map.put(:username, username)
       |> Map.put(:productName, product_name)
 
-    "/checkout/request"
-    |> post(params)
-    |> process_result()
+    case validate_attrs(params) do
+      {:ok} ->
+        "/checkout/request"
+        |> post(params)
+        |> process_result()
+
+      {:error, message} ->
+        message
+    end
+  end
+
+  defp validate_attrs(attrs) do
+    cond do
+      Map.has_key?(attrs, :amount) === false ->
+        {:error, "The request is missing required member 'amount'"}
+
+      Map.has_key?(attrs, :phoneNumber) === false ->
+        {:error, "The request is missing required member 'phoneNumber'"}
+
+      Map.has_key?(attrs, :currencyCode) === false ->
+        {:error, "The request is missing required member 'currencyCode'"}
+
+      Map.has_key?(attrs, :providerChannel) && is_bitstring(attrs.providerChannel) === false ->
+        {:error, "The required member 'providerChannel' must be a string"}
+
+      Map.has_key?(attrs, :metadata) && is_map(attrs.metadata) === false ->
+        {:error, "The required member 'metadata' must be a map"}
+
+      true ->
+        {:ok}
+    end
   end
 end
