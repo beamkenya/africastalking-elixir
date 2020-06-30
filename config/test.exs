cb727580d268f7d8391d19c711e4e91bf980b917
manuelgeek <emashmagak@appslab.co.ke>
2020-06-30 16:18:21 +0300

    added mpesa checkput

--- config/test.exs
+++ config/test.exs
@@ -6,4 +6,5 @@ config :at_ex,
   api_key: "6f2a021cbd7b2b0f5e9ba324f42dbf42403ab9a239d1c55c5a68cf54922bda77",
   content_type: "application/json",
   accept: "application/json",
-  auth_token: ""
+  auth_token: "",
+  product_name: "AtEx"
--- lib/at_ex/gateway/Payments/Mobile/checkout.ex
+++ lib/at_ex/gateway/Payments/Mobile/checkout.ex
@@ -1,23 +1,18 @@
 defmodule AtEx.Gateway.Payments.Mobile.Checkout do
   @moduledoc false
 
-  use AtEx.Gateway.Base
-
-  @live_url "https://payments.africastalking.com/mobile/checkout/request"
-  @sandbox_url "https://payments.sandbox.africastalking.com/mobile/checkout/request"
-
   # Using this system for delivery of which URL to use (sandbox or live)
   # determined by whether we are in production or development or test
   # Selection of the live URL can be forced by setting an environment
   # variable FORCE_AT_LIVE=YES
-  defp get_url do
-    cond do
-      Mix.env() == :prod -> @live_url
-      System.get_env("FORCE_AT_LIVE") == "YES" -> @live_url
-      true -> @sandbox_url
+  url =
+    if Mix.env() == :prod || System.get_env("FORCE_AT_LIVE") == "YES" do
+      "https://payments.africastalking.com/mobile"
+    else
+      "https://payments.sandbox.africastalking.com/mobile"
     end
-  end
 
+  use AtEx.Gateway.Base, url: url, type: "json"
 
   @doc """
   This function initiates a mobile checkout request by sending a HTTP POST request to the Africa's talking Mobile Checkout endpoint.
@@ -35,8 +30,9 @@ defmodule AtEx.Gateway.Payments.Mobile.Checkout do
       |> Map.put(:username, username)
       |> Map.put(:productName, product_name)
 
-    get_url()
+    "/checkout/request"
     |> post(params)
+    |> IO.inspect
     |> process_result()
   end
 end
