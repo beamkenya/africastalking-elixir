e0ab355906090eae1a8a8420b37f4b101c2ff1fb
manuelgeek <emashmagak@appslab.co.ke>
2020-07-01 19:05:23 +0300

    added tests for b2c

--- test/at_ex/gateway/Payments/Mobile/b2c_test.ex
+++ test/at_ex/gateway/Payments/Mobile/b2c_test.ex
@@ -3,4 +3,109 @@ defmodule AtEx.Gateway.Payments.Mobile.B2cTest do
 
   use ExUnit.Case
   alias AtEx.Gateway.Payments.Mobile.B2c
+
+  setup do
+    Tesla.Mock.mock(fn
+      %{method: :post} ->
+        %Tesla.Env{
+          status: 201,
+          body:
+            Jason.encode!(%{
+              "entries" => [
+                %{
+                  "phoneNumber" => "+254724540000",
+                  "provider" => "Athena",
+                  "providerChannel" => "525900",
+                  "status" => "Queued",
+                  "transactionFee" => "KES 0.1000",
+                  "transactionId" => "ATPid_beeb0be6b1bff57ec8f32675fe3f6e72",
+                  "value" => "KES 10.0000"
+                }
+              ],
+              "numQueued" => 1,
+              "totalTransactionFee" => "KES 0.1000",
+              "totalValue" => "KES 10.0000"
+            })
+        }
+    end)
+
+    :ok
+  end
+
+  describe "B2c Checkout" do
+    test "b2c_checkout/1 should initiate b2c checkout  successfully" do
+      details = [
+        %{
+          phoneNumber: "254724540000",
+          amount: 10,
+          currencyCode: "KES",
+          providerChannel: "525900",
+          metadata: %{message: "I am here"}
+        }
+      ]
+
+      {:ok, result} = B2c.b2c_checkout(details)
+      assert 1 = result["numQueued"]
+      assert is_list(result["entries"])
+    end
+
+    test "b2c_checkout/1 should fail without required params 'phoneNumber' in the recipient list item map" do
+      details = [
+        %{
+          amount: "254724540000",
+          currencyCode: "KES",
+          providerChannel: "525900",
+          metadata: %{message: "I am here"}
+        }
+      ]
+
+      {:error, message} = B2c.b2c_checkout(details)
+
+      assert "The request is missing required member 'phoneNumber' in one of te recipients" =
+               message
+    end
+
+    test "b2c_checkout/1 should fail without required params 'amount' in the recipient list item map" do
+      details = [
+        %{
+          phoneNumber: 10,
+          currencyCode: "KES",
+          providerChannel: "525900",
+          metadata: %{message: "I am here"}
+        }
+      ]
+
+      {:error, message} = B2c.b2c_checkout(details)
+      assert "The request is missing required member 'amount' in one of te recipients" = message
+    end
+
+    test "b2c_checkout/1 should fail without required params 'currencyCode' in the recipient list item map" do
+      details = [
+        %{
+          phoneNumber: "254724540000",
+          amount: 10,
+          metadata: %{message: "I am here"}
+        }
+      ]
+
+      {:error, message} = B2c.b2c_checkout(details)
+
+      assert "The request is missing required member 'currencyCode' in one of te recipients" =
+               message
+    end
+
+    test "b2c_checkout/1 should fail without required param 'metadata' is not a map" do
+      details = [
+        %{
+          phoneNumber: "254724540000",
+          amount: 10,
+          currencyCode: "KES",
+          metadata: "a string"
+        }
+      ]
+
+      {:error, message} = B2c.b2c_checkout(details)
+      assert "The required member 'metadata' must be a map in one of te recipients" = message
+    end
+  end
 end
