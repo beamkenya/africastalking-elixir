97ef8e9fdc9b15434aacb2590a7479d189a54b76
manuelgeek <emashmagak@appslab.co.ke>
2020-07-14 17:06:27 +0300

    added bank transfer

--- config/dev.sample.exs
+++ config/dev.sample.exs
@@ -12,5 +12,6 @@ config :at_ex,
   stk_product_name: "AtEx",
   b2c_product_name: "AtEx",
   bank_checkout_product_name: "AtEx",
+  bank_transfer_product_name: "AtEx",
   # When changed to "YES" one will use the live endpoint url
   force_live_url: "NO"
