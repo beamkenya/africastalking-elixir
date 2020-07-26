ed52f721982ff34c64d29e4255b31a522e556169
kamalogudah <mcpaul2058@gmail.com>
2020-07-26 12:41:11 +0300

    Work on UploadMedia Voice functionality

--- config/test.exs
+++ config/test.exs
@@ -3,12 +3,13 @@ use Mix.Config
 config :tesla, adapter: Tesla.Mock
 
 config :at_ex,
-  api_key: "6f2a021cbd7b2b0f5e9ba324f42dbf42403ab9a239d1c55c5a68cf54922bda77",
-  content_type: "application/json",
+  api_key: "7c8000c4551778e357d95588bda259a0122d6f9b457c09e951984c393ccfd9f8",
+  content_type: "application/x-www-form-urlencoded",
   accept: "application/json",
   auth_token: "",
   stk_product_name: "AtEx",
   b2c_product_name: "AtEx",
   b2b_product_name: "AtEx",
   bank_checkout_product_name: "AtEx",
-  bank_transfer_product_name: "AtEx"
+  bank_transfer_product_name: "AtEx",
+  force_live_url: "No"
